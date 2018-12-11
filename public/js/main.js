f87fd990cebc538a81a8732569657c65788272f9
solomon-xpo <solomon.periasamy@xpo.com>
2018-12-11 16:50:10 +0530

    updated sidebar

--- public/js/main.js
+++ public/js/main.js
@@ -34,7 +34,14 @@ $(function () {
             $("#ordercard").hide();
         }
     });
+    
 
 });
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
 
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
 
